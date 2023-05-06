0c14f89bbd27c49cc0915f5544948af58012ff7d
Folagbade Olowofela <89777325+Marvel-Deone@users.noreply.github.com>
2023-05-06 11:48:15 +0100

    dev commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,53 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SidebarComponent } from './public/sidebar/sidebar.component';
-import { HeaderComponent } from './public/header/header.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { SignInComponent } from './auth/sign-in/sign-in.component';
-import { SignUpComponent } from './auth/sign-up/sign-up.component';
-import { ProfileComponent } from './profile/profile.component';
-import { NotificationComponent } from './notification/notification.component';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { en_US } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-import { NzNotificationModule } from 'ng-zorro-antd/notification';
-import { EmergencyInformationComponent } from './emergency-information/emergency-information.component';
-
-registerLocaleData(en);
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
+import { AppComponent } from './app.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
 @NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ComponentsModule,
+    RouterModule,
+    AppRoutingModule,
+  ],
   declarations: [
     AppComponent,
-    SidebarComponent,
-    HeaderComponent,
-    DashboardComponent,
-    SignInComponent,
-    SignUpComponent,
-    ProfileComponent,
-    NotificationComponent,
-    EmergencyInformationComponent,
+    AdminLayoutComponent,
+
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    MatSnackBarModule,
-    FormsModule,
-    NzNotificationModule
-  ],
-  providers: [
-    { provide: NZ_I18N, useValue: en_US }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-
-    Url: 'https://medaid-api.vercel.app/api',
-    baseUrl: 'https://medaid-api.vercel.app/api'
-};
